f7e3601a68d691bf018471635e5eb607b3d13a34
Mr_tang <18242312660@163.com>
2018-06-19 11:53:55 +0800

    系统刷新控件存在的问题

--- RefreshContrlAndKVO/ViewController.swift
+++ RefreshContrlAndKVO/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    lazy var refreshCtrl = UIRefreshControl()
+    
+    /**
+     如果用户不放手，下拉到一定程度，会进入自动刷新状态，浪费流量
+     如果程序主动调用beginRefreshing()，不显示菊花转动器，这是Xcode8.0出现的
+     自定义刷新控件，最重要要解决的就是用户放手，不再刷新
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //添加刷新控件
+        tableView.addSubview(refreshCtrl)
+        refreshCtrl.addTarget(self, action: #selector(loadData), for: .valueChanged)
+    }
+    @objc func loadData() {
+        print("开始刷新")
+        refreshCtrl.beginRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            print("结束刷新")
+            self.refreshCtrl.endRefreshing()
+        }
     }
 
     override func didReceiveMemoryWarning() {
