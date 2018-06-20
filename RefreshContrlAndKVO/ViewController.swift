772030fcaad9315a707032901b4c8e8b9df2b60b
Mr_tang <18242312660@163.com>
2018-06-20 11:49:43 +0800

    调整

--- RefreshContrlAndKVO/ViewController.swift
+++ RefreshContrlAndKVO/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //设置contentInset
+        tableView.contentInsetAdjustmentBehavior = .never
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
         //添加刷新控件
         tableView.addSubview(refreshCtrl)
